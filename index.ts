36817d1b736c1f8fb44dcdc3ef177af18e9ef8cc
kawzaiphophu <156281540+kawzaiphophu@users.noreply.github.com>
2024-04-22 11:57:48 +0700

    fix db

--- index.ts
+++ index.ts
@@ -3,18 +3,33 @@ import path from 'path';
 import { myDataSource } from './src/config/dataSource';
 import router from './src/route/router';
 
+let isConnected = false; 
+
 async function connectDatabase() {
     try {
-        await myDataSource.initialize();
-        console.log("Data Source Connected!");
+        if (!isConnected) {
+            await myDataSource.initialize();
+            console.log("Data Source Connected!");
+            isConnected = true; 
+        } else {
+            console.log("Data Source is already connected.");
+        }
     } catch (err) {
         console.error("Error Connect data source:", err);
     }
 }
-connectDatabase();
 
 const app = express();
 const PORT = 3001;
+
+app.use(async (req, res, next) => {
+    try {
+        await connectDatabase();
+        next();
+    } catch (err) {
+        next(err);
+    }
+});
 
 app.set('views', path.join(__dirname, './src/views'));
 app.set('view engine', 'ejs');
